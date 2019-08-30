b20c1efba5848699a21600eb8c4c44166f4f34dd
ihernandez4466 <54653583+ihernandez4466@users.noreply.github.com>
2019-08-30 12:00:44 -0700

    Add files via upload
    homework 3

---
+++ Untitled6.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("in order to feel valued in this class I will hopefully understand the majority of the material")
+
+
+# In[ ]:
+
+
+
+
